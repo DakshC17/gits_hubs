fa109925c1e43131829cab9df056c2416e880e69
DakshC17 <dakshc1709gmail.com>
2024-09-06 19:26:56 +0530

    hey everyone this is my first commits

---
+++ L3.cpp
@@ -0,0 +1,80 @@
+#include<iostream>
+using namespace std;
+int main()
+/*{
+    int a = 10;
+    int b = 23;
+    if (a>b)
+        cout<<" a is bigger";
+    else;
+        cout<<" a is smaller";
+}*/
+
+
+
+    /*{float a,b,c;
+    cout<<"Enter three numbers that you want to compare:"<<endl;
+    cin>>a>>b>>c;
+    if((a>b)&&(a>c))
+        cout<<"a is greater"<<endl;
+    else if((b>a)&&(b>c))
+        cout<<"b is greater"<<endl;
+    else
+        cout<<"c is greater"<<endl;
+    }*/
+
+    /*int n;
+    cout<<"ENter any number to check if the number is +,- or 0;"<<endl;
+    cin>>n;
+    if(n>0)
+        cout<<"Positive"<<endl;
+    else if(n<0)
+        cout<<"The number is negative"<<endl;
+    else
+        cout<<"Zero"<<endl;}*/
+
+
+/*{
+    char ch;
+    cout<<"Enter any character"<<endl;
+    cin>>ch;
+    if((ch>='a')&&(ch<='z'))
+        cout<<"Lower case"<<endl;
+    else if((ch>='A')&&(ch<='Z'))
+        cout<<"upper case"<<endl;
+    else if((ch>='0')&&(ch<='9'))
+        cout<<"Numeric"<<endl;
+
+}*/
+
+/*{
+
+    int n;
+    cout<<"enter any number upto which you want to calculate the sum"<<endl;
+    cin>>n;
+    int i=1,sum=0;
+    while(i<=n){
+        sum=sum+i;
+       i++;
+    }
+    cout<<"Sum of thee given range is"<<sum<<endl;
+}*/
+int main()
+{
+    int n;
+    cout<<"Enter a number";
+    cin>>n;
+    int i=1;
+    char ch='A';
+    while(i<=n)
+    {
+        int j=1;
+        while(j<=n)
+        {
+            cout<<"ch";
+            j++; 
+        } 
+       ch++;
+       i++;
+    }
+}
---
+++ L4.cpp
@@ -0,0 +1,143 @@
+#include<iostream>
+using namespace std;
+
+
+
+
+// int main()
+// {
+//     int n;
+//     cin>>n;
+//     int count=1;
+//     int i=1;
+//      while(i<=n){
+//         int j=1;
+//         while(j<=n){
+//             cout<<count<<" ";
+//             count++;
+//             j++;
+//         }
+//         cout<<endl;
+//         i++;
+//      }
+
+
+// }
+
+
+
+// int main()
+// {
+//     int n;
+//     cin>>n;
+//     // int count=1;
+//     int i=1;
+//      while(i<=n){
+//         int j=1;
+//         while(j<=i){
+//             cout<<"*"<<" ";
+//             j++;
+//         }
+//         cout<<endl;
+//         i++;
+//      }
+
+
+// }
+
+// 
+
+
+
+
+
+// int main()
+// {
+//     int n;
+//     cin>>n;
+
+//     int i=1;
+//     int count=1;
+//     while(i<=n){
+//         int j=1;
+//         while(j<=i){
+//             cout<<count<<" ";
+//             count++;
+//             j++;
+//         }
+//         cout<<endl;
+//         i++;
+//     }
+// }
+
+
+// int main(){
+//     int n;
+//     cin>>n;
+//     int count=0;
+//     int i=1;
+//     while(i<=n){
+//         int j=1;int value=i;
+//         while(j<=i){
+//             cout<<i-j+1;
+//             value++;
+//             j++;
+            
+//         }cout<<endl;
+//         i++;
+//     }
+// }
+
+
+// int main(){
+//     int n;
+//     cin>>n;
+//     int i=1;
+//     while(i<=n){
+//         int j=1;
+//         while(j<=n);{
+//         if(i==1)
+//             cout<<"A"<<" ";
+//         else if(i==2)
+//             cout<<"B"<<" ";
+//         else
+//             cout<<"C"<<" ";
+//        j++; } cout<<"endl";
+//        i++;
+//     }
+// }
+// 
+// int main(){
+//     int a=2;
+//     int b=3;
+//     int c=a+b;
+//     cout<<c;
+// }
+
+// int main(){
+//     char ch='A';
+//     ch++;
+//     cout<<ch;
+// }
+
+
+int main()
+{
+    int n;
+    cout<<"Enter a number";
+    cin>>n;
+    int i=1;
+    char ch='A';
+    while(i<=n)
+    {
+        int j=1;
+        while(j<=n)
+        {
+            cout<<ch<<" ";
+            j++; 
+        }
+        cout<<endl; 
+       ch++;
+       i++;
+    }
+}
